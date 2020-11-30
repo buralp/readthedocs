2bce912059e604a20c14690cc5b06bbf0b4499b9
alperburhan <ab@inensus.com>
2020-11-30 16:24:57 +0300

    ekleme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 
 
 # -- Project information -----------------------------------------------------
